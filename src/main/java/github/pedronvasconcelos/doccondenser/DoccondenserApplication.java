3d03197f8df86901c0e890de4cc74360714ffa22
pedronvasconcelos <pedrohnvasconcelos@gmail.com>
2024-04-06 21:21:40 -0300

    First commit.

---
+++ src/main/java/github/pedronvasconcelos/doccondenser/DoccondenserApplication.java
@@ -0,0 +1,13 @@
+package github.pedronvasconcelos.doccondenser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoccondenserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoccondenserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/github/pedronvasconcelos/doccondenser/DoccondenserApplicationTests.java
@@ -0,0 +1,13 @@
+package github.pedronvasconcelos.doccondenser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoccondenserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
